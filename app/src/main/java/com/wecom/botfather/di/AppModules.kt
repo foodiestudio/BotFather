7732d9abd114aed73b8765ea4ded7e7575901a18
江辰 <chen.jiang@tuya.com>
2022-01-05 15:38:32 +0800

    feat: 设置项新增控制是否请求，方便调试用
    - 玩了下 DataStore
    - 成功动态更新 koin 的依赖

--- app/src/main/java/com/wecom/botfather/di/AppModules.kt
+++ app/src/main/java/com/wecom/botfather/di/AppModules.kt
@@ -3,7 +3,6 @@ package com.wecom.botfather.di
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import com.squareup.sqldelight.db.SqlDriver
 import com.wecom.botfather.Database
-import com.wecom.botfather.mock.MockService
 import com.wecom.botfather.sdk.WeComBotHelper
 import com.wecom.botfather.sdk.service.WeComService
 import com.wecom.botfather.ui.chat.ChatViewModel
@@ -14,27 +13,12 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val debug = true
-
-
 val uiModules = module {
     viewModel { HomeViewModel(get()) }
     viewModel { ChatViewModel(get()) }
 }
 
 val sdkModules = module {
-    if (debug) {
-        // 使用日志打印代替请求
-        single<WeComService> { MockService() }
-    } else {
-        single<WeComService> {
-            Retrofit.Builder()
-                .baseUrl("https://qyapi.weixin.qq.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(WeComService::class.java)
-        }
-    }
 
     single<SqlDriver> {
         AndroidSqliteDriver(
@@ -48,8 +32,19 @@ val sdkModules = module {
         Database(get())
     }
 
-    single { WeComBotHelper(get<Database>().botQueries) }
+    factory { WeComBotHelper(get<Database>().botQueries) }
 
 }
 
-val appModules = sdkModules + uiModules
+val serviceModule = module {
+    single<WeComService> {
+        Retrofit.Builder()
+            .baseUrl("https://qyapi.weixin.qq.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeComService::class.java)
+    }
+}
+
+val appModule = uiModules + sdkModules + serviceModule
+
