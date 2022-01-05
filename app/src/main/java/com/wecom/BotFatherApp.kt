e8610856bb9dabda8024c32b373dbba6b3896b61
江辰 <chen.jiang@tuya.com>
2022-01-05 15:38:32 +0800

    feat: 设置项新增控制是否请求，方便调试用
    - 玩了下 DataStore
    - 成功动态更新 koin 的依赖

--- app/src/main/java/com/wecom/BotFatherApp.kt
+++ app/src/main/java/com/wecom/BotFatherApp.kt
@@ -1,15 +1,34 @@
 package com.wecom
 
 import android.app.Application
-import com.wecom.botfather.di.appModules
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.wecom.botfather.BuildConfig
+import com.wecom.botfather.di.appModule
+import com.wecom.botfather.di.serviceModule
+import com.wecom.botfather.mock.MockService
+import com.wecom.botfather.sdk.service.WeComService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.Koin
+import org.koin.core.context.KoinContext
+import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
+import org.koin.core.context.unloadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
 import timber.log.Timber
 
 typealias T = Timber
 
-class BotFatherApp : Application() {
+class BotFatherApp : Application(), CoroutineScope by MainScope() {
 
     override fun onCreate() {
         super.onCreate()
@@ -17,9 +36,39 @@ class BotFatherApp : Application() {
         T.tag(this::class.java.simpleName)
 
         startKoin {
+            allowOverride(true)
             androidContext(this@BotFatherApp)
             androidLogger()
-            modules(appModules)
+            modules(appModule)
+        }
+
+        launch {
+            // 监听变更
+            dataStore.data.collectLatest {
+                loadServiceModule(it[ABORT_REQUEST_KEY] ?: BuildConfig.DEBUG)
+            }
+        }
+    }
+
+    companion object {
+        private val debugModule = module { single<WeComService> { MockService() } }
+
+        /**
+         * Koin 允许加载同类的 module，覆盖之前的
+         */
+        fun loadServiceModule(debug: Boolean) {
+            if (debug) {
+                loadKoinModules(debugModule)
+            } else {
+                unloadKoinModules(debugModule)
+                loadKoinModules(serviceModule)
+            }
         }
     }
 }
+
+// At the top level of your kotlin file:
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+// abort api request
+val ABORT_REQUEST_KEY = booleanPreferencesKey("abort_request")
--- app/src/main/java/com/wecom/botfather/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/wecom/botfather/ui/settings/SettingsActivity.kt
@@ -5,10 +5,24 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.datastore.preferences.core.edit
+import com.wecom.ABORT_REQUEST_KEY
+import com.wecom.botfather.BuildConfig
+import com.wecom.dataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 /**
  * TODO
@@ -18,8 +32,18 @@ class SettingsActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val prefValue = dataStore.data.map {
+            it[ABORT_REQUEST_KEY] ?: BuildConfig.DEBUG
+        }
         setContent {
-            SettingScreen()
+            val scope = rememberCoroutineScope()
+            SettingScreen(prefValue) { newValue ->
+                scope.launch {
+                    dataStore.edit {
+                        it[ABORT_REQUEST_KEY] = newValue
+                    }
+                }
+            }
         }
     }
 
@@ -34,7 +58,9 @@ class SettingsActivity : ComponentActivity() {
 }
 
 @Composable
-private fun SettingScreen() {
+private fun SettingScreen(prefValue: Flow<Boolean>, onValueChange: (Boolean) -> Unit) {
+    val abortRequest by prefValue.collectAsState(false)
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -42,7 +68,21 @@ private fun SettingScreen() {
             )
         },
         content = {
-
+            SwitchPreference(
+                title = { Text("禁用请求") },
+                description = {
+                    Text(
+                        text = "不进行网络请求，改为 Log 输出",
+                        fontSize = 12.sp,
+                        color = Color.Gray
+                    )
+                },
+                checked = abortRequest,
+                onCheckedChange = onValueChange,
+                modifier = Modifier.clickable {
+                    onValueChange(!abortRequest)
+                }
+            )
         }
     )
 }
